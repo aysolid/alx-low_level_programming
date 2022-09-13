1bfca254d9db10a879a8716e0b61189d6234beea
aysolid <davidogundairo@gmail.com>
2022-09-13 10:25:55 -0700

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
+ * main - print fibonacci
+ *
+ * Return: always 0
  */
 int main(void)
 {
-int i = 0;
-unsigned long int a = 0, b = 1, next = 0;
+unsigned long int a, a1, a2, b, b1, b2, c, c1, c2, d, e;
 
-while (i < 98)
+a = 1;
+b = 2;
+c = a + b;
+
+printf("%lu, ", a);
+printf("%lu, ", b);
+for (d = 3; d < 89; d++)
 {
-next = a + b;
+printf("%lu, ", c);
 a = b;
-b = next;
-printf("%lu", next);
-if (i < 97)
-printf(", ");
-i++;
+b = c;
+c = a + b;
 }
-putchar('\n');
+b1 = b / 1000000000;
+b2 = b % 1000000000;
+c1 = c / 1000000000;
+c2 = c % 1000000000;
+for (e = 89; e < 98; e++)
+{
+printf("%lu%lu, ", c1, c2);
+a1 = b1;
+a2 = b2;
+b1 = c1;
+b2 = c2;
+c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+c2 = (a2 + b2) % 1000000000;
+}
+printf("%lu%lu\n", c1, c2);
 return (0);
 }
